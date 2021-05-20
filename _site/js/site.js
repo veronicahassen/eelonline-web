b4dece791ad80f95b493d9e99cdfb53b6229b878
German Mazza <ger.mazzagentile@gmail.com>
2021-05-20 17:20:15 -0300

    Quito carpeta _site

--- _site/js/site.js
+++
@@ -1,8 +0,0 @@
-function navigateToUrl(url) {
-    if ((window.location.origin + "/" !== window.location.href) && window.location.href !== window.location.origin + "/#content") {
-        window.location.replace(window.location.origin + url);
-    }
-}
-
-
-
